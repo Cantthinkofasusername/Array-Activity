f987285be1f597ac531cad06ad862a7bbf33e319
Your Name <shado@LAPTOP-U7GG9IKM>
2024-10-15 19:28:31 +0800

    initial commit

---
+++ arrayActivity.js
@@ -0,0 +1,12 @@
+let favSingers = ["Ed Sheeran", "Thomas Jacob Black", "Avril Lavona Lavigne"];
+
+console.log(favSingers[0])
+
+favNumbers = [1, 2, 3, 4];
+
+mixedArr = ["word", ["word1", "word2"], 987, false];
+
+console.log(mixedArr[0]);
+console.log(mixedArr[1]);
+console.log(mixedArr[2]);
+console.log(mixedArr[3]);
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+mixedArr = ["word", ["word1", "word2"], 987, false];
+
+console.log(mixedArr[0]);
+console.log(mixedArr[1]);
+console.log(mixedArr[2]);
+console.log(mixedArr[3]);
